582854370b730360998dc603f008758ecc846645
Brian Duddie <bduddie@gmail.com>
2014-06-28 19:14:12 -0700

    Write response body to file

--- scraper/spiders/xda_spider.py
+++ scraper/spiders/xda_spider.py
@@ -1,12 +1,14 @@
 import scrapy
 
 class XdaSpider(scrapy.Spider):
-    name = "xda-developers"
+    name = "xda"
     allowed_domains = "forum.xda-developers.com"
     start_urls = [
         "http://forum.xda-developers.com/google-nexus-5/general"
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2]
-        print(filename)
+        uri_split = response.url.split("/")
+        filename = uri_split[-1]
+        with open(filename, 'wb') as file:
+            file.write(response.body)
